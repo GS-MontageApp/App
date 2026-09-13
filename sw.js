60d46a46305e41b19264f7a11921d3b038a713cc
GS-MontageApp <m3-jost@gmx.de>
2026-09-13 12:56:51 +0200

    Update cache version and add new image assets

--- sw.js
+++ sw.js
@@ -1,11 +1,28 @@
-const CACHE_NAME = 'zangenschlosser-app-v1.10.32';
+const CACHE_NAME = 'zangenschlosser-app-v1.10.35';
 const urlsToCache = [
   './index.html',
   './changelogs.js',
   './manifest.json',
   'https://cdn.tailwindcss.com',
   './img/icon-512.png',
-  './img/dr-zange.jpg'
+  './img/dr-zange.jpg',
+  './img/a-0.png',
+  './img/a-45.png',
+  './img/a-90.png',
+  './img/a-135.png',
+  './img/a-180.png',
+  './img/a-225.png',
+  './img/a-270.png',
+  './img/a-315.png',
+  './img/a-f.png',
+  './img/b-0.png',
+  './img/b-45.png',
+  './img/b-90.png',
+  './img/b-135.png',
+  './img/b-180.png',
+  './img/b-225.png',
+  './img/b-270.png',
+  './img/b-315.png'
 ];
 
 self.addEventListener('install', (event) => {
