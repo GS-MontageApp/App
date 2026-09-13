adbed172d09478f0c48892c4107969b4e1a7dde7
GS-MontageApp <m3-jost@gmx.de>
2026-09-13 21:03:10 +0200

    Update build date in version.js

--- js/version.js
+++ js/version.js
@@ -3,7 +3,7 @@
 // ============================================================================
 const APP_CONFIG = {
   version: 'v1.10.54',
-  date: '13.09.2026, 20:55 (MEZ)'
+  date: '13.09.2026, 21:30 (MEZ)'
 };
 
 if (typeof window !== 'undefined') window.APP_CONFIG = APP_CONFIG;
