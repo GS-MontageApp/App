b5d4c6def8f0976efe3e10aad2cc984fd8444cbe
GS-MontageApp <m3-jost@gmx.de>
2026-09-13 13:25:54 +0200

    Update service worker cache version and cleanup

--- sw.js
+++ sw.js
@@ -1,35 +1,15 @@
-const CACHE_NAME = 'zangenschlosser-app-v1.10.32';
+const CACHE_NAME = 'zangenschlosser-app-v1.10.36';
 const urlsToCache = [
   './index.html',
   './changelogs.js',
   './manifest.json',
   'https://cdn.tailwindcss.com',
   './img/icon-512.png',
-  './img/dr-zange.jpg',
-  './img/a-0.png',
-  './img/a-45.png',
-  './img/a-90.png',
-  './img/a-135.png',
-  './img/a-180.png',
-  './img/a-225.png',
-  './img/a-270.png',
-  './img/a-315.png',
-  './img/a-f.png',
-  './img/b-0.png',
-  './img/b-45.png',
-  './img/b-90.png',
-  './img/b-135.png',
-  './img/b-180.png',
-  './img/b-225.png',
-  './img/b-270.png',
-  './img/b-315.png'
+  './img/dr-zange.jpg'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
@@ -38,9 +18,7 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
         })
       );
     })
@@ -50,7 +28,6 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
