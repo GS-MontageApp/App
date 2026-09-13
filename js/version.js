c2c75fc77a651f5020856dd29aa041bfa23a8813
GS-MontageApp <m3-jost@gmx.de>
2026-09-13 18:38:19 +0200

    Update version and date in APP_CONFIG

--- js/version.js
+++ js/version.js
@@ -2,8 +2,8 @@
 // SINGLE SOURCE OF TRUTH: VERSION & BUILD
 // ============================================================================
 const APP_CONFIG = {
-  version: 'v1.10.41',
-  date: '13.09.2026, 16:20 (MEZ)'
+  version: 'v1.10.42',
+  date: '13.09.2026, 18:30 (MEZ)'
 };
 
 // Global export for Window (Browser) and ServiceWorker (self)
