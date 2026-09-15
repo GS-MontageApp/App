470a1ce96ea5f45343086fe1a9d46dd565643bd6
GS-MontageApp <m3-jost@gmx.de>
2026-09-15 22:29:03 +0200

    Update version to v1.10.91

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 // SINGLE SOURCE OF TRUTH: VERSION & BUILD (v1.10.68)
 // ============================================================================
 const APP_CONFIG = {
-  version: 'v1.10.90',
+  version: 'v1.10.91',
   date: '15.09.2026, 19:25 (MEZ)'
 };
 
