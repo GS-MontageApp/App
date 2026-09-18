674e5e211d6aa17b5e3de1cf44cbe7acf4ce2545
GS-MontageApp <m3-jost@gmx.de>
2026-09-18 13:32:25 +0200

    Update service worker version and cache name

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 // ============================================================================
-// ZANGENSCHLOSSER-APP: SERVICE WORKER (v1.10.119)
+// ZANGENSCHLOSSER-APP: SERVICE WORKER (v1.10.121)
 // ============================================================================
-const CACHE_NAME = 'zangenschlosser-cache-v1.10.119';
+const CACHE_NAME = 'zangenschlosser-cache-v1.10.121';
 const ASSETS = [
   './',
   './index.html',
+  './css/style.css',
   './js/version.js',
   './js/shell.js',
   './js/etagen.js',
@@ -13,7 +14,8 @@ const ASSETS = [
   './js/tabellen.js',
   './js/drehmoment.js',
   './manifest.json',
-  './img/dr-zange.jpg'
+  './img/dr-zange.jpg',
+  './img/eo-form.png'
 ];
 
 self.addEventListener('install', (event) => {
