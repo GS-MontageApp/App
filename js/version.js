ef2464c1cef2787f09b3f4bc4001a8e472874931
GS-MontageApp <m3-jost@gmx.de>
2026-09-19 18:19:11 +0200

    Update build date in version.js
    Updated the build date in APP_CONFIG to reflect the new time.

--- js/version.js
+++ js/version.js
@@ -3,9 +3,8 @@
 // ============================================================================
 const APP_CONFIG = {
   version: 'v1.12.1',
-  date: '19.09.2026, 18:00 (MEZ)'
+  date: '19.09.2026, 18:30 (MEZ)'
 };
 
-// Global export for Window (Browser) and ServiceWorker (self)
 if (typeof window !== 'undefined') window.APP_CONFIG = APP_CONFIG;
 if (typeof self !== 'undefined') self.APP_CONFIG = APP_CONFIG;
